7783d84b59329a48e3338bd46e472ea2999d5eb3
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-11-10 23:54:23 -0500

    Create react-component.js

---
+++ react-component.js
@@ -0,0 +1,22 @@
+/* Anatomy of a React Component */ 
+	Class Post extends React.Component {
+	    state = {
+	        title: 'First Post',
+	        body: 'This is my first post.'
+	    }
+
+	    render() {
+	        return {
+	            <div>
+	                <h3>{ this.state.title }</h3>
+	                <p>{ this.state.body }</p>
+	            </div>
+	        }
+	    }
+	}
+  
+ /* 
+React functional components as well. In this code, we have a class called ‘Post’, so it’s a ‘Post’ component. It extends the component class in the React library. 
+And we have our render() method (which is the only required lifeylle method). This method takes care of rendering the component on the page and returning JSX. JavaScript expressions are embedded directly in the markup. 
+Component states are created by creating a state object with different key pairs. Any JavaScript expressions can be included inside of the curly braces.
+*/
